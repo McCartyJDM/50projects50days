cf6cd388c5b674d4c5499d35763e26774a787946
John McCarty <mccarty.jdm@gmail.com>
2022-03-21 21:55:56 -0400

    Updated to add click function back

--- 20-button-ripple-effect/README.md
+++ 20-button-ripple-effect/README.md
@@ -1,3 +1,3 @@
 # Button Ripple Effect
 ## Day 20 - Sunday Mar. 20th, 2022
-I altered the Button Ripple Effect exercise
+I created the Button Ripple Effect exercise.
--- 20-button-ripple-effect/script.js
+++ 20-button-ripple-effect/script.js
@@ -4,12 +4,12 @@ buttons.forEach(button => {
     button.addEventListener('click', function (e) {
         const x = e.clientX
         const y = e.clientY
-        
+
         const buttonTop = e.target.offsetTop
         const buttonLeft = e.target.offsetLeft
 
-        const xInside = x - buttonTop
-        const yInside = y - buttonLeft
+        const xInside = x - buttonLeft
+        const yInside = y - buttonTop
 
         const circle = document.createElement('span')
         circle.classList.add('circle')
